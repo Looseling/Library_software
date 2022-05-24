e72fae9fd0248270cf37a6e485143577f8ae3c69
Looseling <69507148+Looseling@users.noreply.github.com>
2022-05-24 15:58:04 +0200

    Add files via upload

---
+++ LibControlApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibControlApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
