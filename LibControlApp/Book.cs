b09dbfd6b2c9fd92250b91255897b5fa62add0cd
Looseling <69507148+Looseling@users.noreply.github.com>
2022-06-08 12:34:10 +0200

    Librarian inherited from user

---
+++ LibControlApp/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LibControlApp
+{
+    public class Book
+    {
+        private static int counter = 0;
+        private int id { get; set; }
+        public int pages { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+
+        public string cover { get; set; }
+
+        public Book(string title, string author, string genre, int pages, string cover)
+        {
+            this.id = ++counter;
+            this.pages = pages;
+            this.author = author;
+            this.title = title;
+            this.genre = genre;
+            this.cover = cover;
+        }
+
+        public int GetId()
+        {
+            return this.id;
+        }
+    }
+}
---
+++ LibControlApp/Librarian.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibControlApp
+{
+    public class Librarian : User
+    {
+        public Librarian(string login, string password, string firstName, string lastName)
+            : base(login, password, firstName, lastName)
+        {
+        }
+    }
+}
